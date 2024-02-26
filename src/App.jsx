ac7c4efeeeca529dc2e662f46b2256c64769fc14
Gana <ganafall9498@gmail.com>
2024-02-26 12:37:10 +0100

    Remove unused Nav component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import { RouterProvider } from "react-router-dom";
 import "./App.css";
 import MainNav from "./navigation/MainNav";
-import Nav from "./components/NavComponents/Nav";
 function App() {
   return (
     <>
       <RouterProvider router={MainNav} />
-      <Nav />
     </>
   );
 }
