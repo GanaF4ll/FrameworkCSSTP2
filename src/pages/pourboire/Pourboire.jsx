128a2a3b12552d710d27a9ff5c01c92b8de55811
GanaF4ll <ganafall9498@gmail.com>
2024-02-26 20:13:14 +0100

    Update background colors and add notification styles

--- src/pages/pourboire/Pourboire.jsx
+++ src/pages/pourboire/Pourboire.jsx
@@ -1,16 +1,29 @@
-import { React, useState } from "react";
+import React, { useState } from "react";
 import "./Pourboire.css";
 import HeaderComponent from "../../components/headerComponent/HeaderComponent";
 import BtnComponent from "../../components/btnComponent/BtnComponent";
 import Input from "../../components/input/Input";
 import BtnNumber from "./components/BtnNumber";
+import BtnPourboire from "./components/btnPourboire/BtnPourboire";
 
 const Pourboire = () => {
   const [pourboire, setPourboire] = useState("");
+  const [showNotification, setShowNotification] = useState(false);
+  const [step, setStep] = useState(1);
 
   const handleBtnClick = (value) => {
     console.log(value);
     setPourboire(value);
+  };
+
+  const handleNotificationClick = () => {
+    setShowNotification(true);
+  };
+
+  const handleClick = () => {
+    console.log("Button was clicked"); // Add console.log here
+    setStep((prev) => (prev < 3 ? prev + 1 : prev));
+    setShowNotification(true);
   };
 
   return (
@@ -23,8 +36,13 @@ const Pourboire = () => {
         Montant du pourboire
       </Input>
       <BtnNumber onBtnClick={handleBtnClick} />
-      <BtnComponent title="Ajouter le pourboire" />
+
+      <BtnPourboire title="Ajouter le pourboire" onClick={handleClick} />
+      {showNotification && (
+        <div className="notif flex">Le pourboire a bien été ajouté</div>
+      )}
     </div>
   );
 };
+
 export default Pourboire;
