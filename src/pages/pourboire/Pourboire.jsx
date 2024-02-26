7980b035c83c47764589456d86e41efd16d4cd4a
Gana <ganafall9498@gmail.com>
2024-02-26 16:05:35 +0100

    Refactor input component and update import path

--- src/pages/pourboire/Pourboire.jsx
+++ src/pages/pourboire/Pourboire.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Pourboire.css";
 import HeaderComponent from "../../components/headerComponent/HeaderComponent";
-import Input from "./components/Input";
+import Input from "../../components/input/Input";
 const Pourboire = () => {
   return (
     <div className="pourboire-container">
@@ -9,6 +9,7 @@ const Pourboire = () => {
         Ajouter <br></br> un pourboire
       </HeaderComponent>
       <Input>Nom de table</Input>
+      <Input>Montant du pourboire</Input>
     </div>
   );
 };
