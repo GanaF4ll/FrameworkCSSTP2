0e14b50ae5ec47deab9f9f6f5ffc1675650b5acf
amir-398 <amir.398@hotmail.fr>
2024-02-26 17:02:51 +0100

    Mise à jour du composant BtnComponent et du style associé

--- src/components/btnComponent/BtnComponent.jsx
+++ src/components/btnComponent/BtnComponent.jsx
@@ -1,13 +1,25 @@
+import { NavLink } from "react-router-dom";
 import ToggleSwitchComponent from "../toggleSwitchComponent/ToggleSwitchComponent";
 import "./btnComponent.css";
 export default function BtnComponent({ title, icon, setStep, step }) {
   return (
     <div className="btnComponent">
       {step == 3 && <ToggleSwitchComponent />}
-      <button onClick={() => setStep((prev) => (prev < 3 ? prev + 1 : prev))}>
-        {title}
-        {icon && icon}
-      </button>
+      {step == 3 ? (
+        <NavLink to={"/pourboire"}>
+          <button
+            onClick={() => setStep((prev) => (prev < 3 ? prev + 1 : prev))}
+          >
+            {title}
+            {icon && icon}
+          </button>
+        </NavLink>
+      ) : (
+        <button onClick={() => setStep((prev) => (prev < 3 ? prev + 1 : prev))}>
+          {title}
+          {icon && icon}
+        </button>
+      )}
     </div>
   );
 }
