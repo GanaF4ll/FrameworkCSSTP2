f443e69ab66366f4fa9d44a4cdd73963f898dce7
amir-398 <amir.398@hotmail.fr>
2024-02-26 12:32:29 +0100

    Ajouter le module Luxon et les styles pour le composant d'en-tête

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,13 @@
 import { FaArrowRight } from "react-icons/fa6";
 import BtnComponent from "../../components/btnComponent/BtnComponent";
+import HeaderComponent from "../../components/headerComponent/HeaderComponent";
+import "./Home.css";
 export default function Home() {
   return (
     <div className="homeContainer">
+      <HeaderComponent>
+        Qui travaille <br /> aujourd’hui ?
+      </HeaderComponent>
       <BtnComponent title="Suivant" icon={<FaArrowRight />} />
     </div>
   );
