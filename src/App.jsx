6e20e2b92e70a0157a134cdfcb234dee385f3eaa
Gana <ganafall9498@gmail.com>
2024-02-27 09:04:05 +0100

    pull request spaghetti code deleted

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
-import { useState } from "react";
+import { React, useState } from "react";
 import { Provider } from "react-redux";
-=======
-import React from "react";
->>>>>>> 25f7d213156ac0a620b91a4d66f855fc824094fd
 import { RouterProvider } from "react-router-dom";
 import "./App.css";
 import DrawerComponent from "./components/drawerComponent/DrawerComponent";
